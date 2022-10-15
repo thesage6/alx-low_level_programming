286d032fff82d80aa2950adf6161c8ac44084247
thesage6 <chinahchimene@yahoo.com>
2022-10-14 17:31:13 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main-program entry point.
+ *Return:0 no error, non zero if error.
+ **/
+int main(void)
+{
+  char small;
+  char large;
+
+  for (small = 'a'; small <= 'z'; small++)
+  {
+    putchar(small);
+  }
+  for (large = 'A'; large <= 'Z'; large++)
+  {
+    putchar(large);
+  }
+  putchar('\n');
+  return (0);
+}
